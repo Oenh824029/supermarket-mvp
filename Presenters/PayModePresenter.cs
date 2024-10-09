ced6271b5cd1a383c916b933f1ce5126ec175be8
oscar naranjo <oskarnaranjo_123@outlook.com>
2024-10-09 18:52:25 -0500

    Pay Mode Presenter

---
+++ Presenters/PayModePresenter.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Supermarket_mvp.Models;
+using Supermarket_mvp.Views;
+
+namespace Supermarket_mvp.Presenters
+{
+       internal class PayModePresenter
+    {
+
+        // variables de clase
+        private IPayModeView view;
+        private IPayModeRepository repository;
+        private BindingSource payModeBindingSource;
+        private IEnumerable<PayModeModel> payModeList;
+
+        // constructor de la clase
+        public PayModePresenter(IPayModeView view, IPayModeRepository repository) {
+
+            this.payModeBindingSource = new BindingSource();
+            
+            this.view = view;
+            this.repository = repository;
+
+
+            this.view.SearchEvent += SearchPayMode;
+            this.view.AddNewEvent += AddNewPayMode;
+            this.view.EditEvent   += LoadSelectPayModeToEdit;
+            this.view.SaveEvent   += SavePayMode;
+            this.view.CancelEvent += CancelAction;
+
+            this.view.SetPayModeListBildingSource(payModeBindingSource);
+
+            loadAllPayModeList();
+            this.view.Show();
+        }
+
+
+        private void loadAllPayModeList() {
+
+            payModeList = repository.GetAll();
+            payModeBindingSource.DataSource = payModeList;
+        }
+        private void CancelAction(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SavePayMode(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadSelectPayModeToEdit(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddNewPayMode(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SearchPayMode(object? sender, EventArgs e)
+        {
+            bool emptyValue = string.IsNullOrWhiteSpace(this.view.SearchValue);
+            if (emptyValue == false) {
+                payModeList = repository.GetByValue(this.view.SearchValue);
+            }
+            else
+            {
+                payModeList = repository.GetAll();
+            }
+
+            payModeBindingSource.DataSource = payModeList;
+        }
+
+        // fin de los métodos
+    }
+}
--- Views/PayModeView.cs
+++ Views/PayModeView.cs
@@ -10,12 +10,77 @@ using System.Windows.Forms;
 
 namespace Supermarket_mvp.Views
 {
-    public partial class PayModeView : Form
+    public partial class PayModeView : Form, IPayModeView
     {
+        // agregando propiedades
+        private bool isEdit;
+        private bool isSuccesful;
+        private string message;
         public PayModeView()
         {
             InitializeComponent();
+            AssociateAndRaiseViewEvents();
+
+            tabControl1.TabPages.Remove(tabPagePayModeDetail);
+        }
+
+        // métodos de la clase
+        private void AssociateAndRaiseViewEvents()
+        {
+            BtnSearch.Click += delegate
+            {
+                SearchEvent?.Invoke(this, EventArgs.Empty);
+            };
+
+            TxtSearch.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    SearchEvent?.Invoke(this, EventArgs.Empty);
+                }
+            };
+        }
+        public string PayModeId { 
+            get { return TxtPayModeId.Text; }
+            set { TxtPayModeId.Text = value; }
+            }
+        public string PayModeName {
+            get { return TxtPayModeName.Text; }
+            set { TxtPayModeName.Text = value; }
+        }
+        public string PayModeObservation
+        {
+            get { return TxtPayModeObservation.Text; }
+            set { TxtPayModeObservation.Text = value; }
         }
     
+        public string SearchValue {
+        get { return TxtSearch.Text;  }
+        set { TxtSearch.Text = value; }
+        }
+        public bool IsEdit {
+            get { return isEdit; }
+            set { IsEdit = value;}
+        }
+        public bool IsSuccesful {
+            get { return isSuccesful; }
+            set { IsSuccesful = value; }
+        }
+        public string Message {
+            get { return message; }
+            set { message = value; }
+        }
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public void SetPayModeListBildingSource(BindingSource payModeList)
+        {
+            DgPayMode.DataSource = payModeList;
+        }
     }
 }
