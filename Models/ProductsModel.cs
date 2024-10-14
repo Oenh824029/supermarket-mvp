a4804f5bf5388a55744c001da8aa0d21e8dd3e93
oscar naranjo <oskarnaranjo_123@outlook.com>
2024-10-13 19:44:29 -0500

    Class Products

---
+++ Models/ProductsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket_mvp.Models
+{
+    internal class ProductsModel
+    {
+
+        // atributos de clase
+        public int Products_Id { get; set; }
+        public string Products_Name { get; set; }
+        public int Products_Price { get; set; }
+        public int Products_Stock { get; set; }
+        public int MyProperty { get; set; }
+    }
+}
